98f1300e372b6870509e9ecf40d8aa4500730203
Farhad <farhadg@the-ifc.com>
2024-03-14 23:49:11 +0100

    add jest + bug

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Localization from "expo-localization";
 
 export default function App() {
+  const locales = Localization.getLocales();
+  const languageCode = locales[0].languageCode;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
